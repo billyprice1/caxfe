9ca58e25a39fa6dc888194c68d9a2a6209064cbd
billyprice1 <lkk29898@email.vccs.edu>
2016-09-18 10:49:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ echo
 cd /home/cabox/workspace/
 /home/cabox/workspace/noVNC/utils/launch.sh --vnc localhost:5900
 cd /home/cabox/workspace/caxfe/ supervisord -c supervisord.conf
+cd /home/cabox/workspace/cabox
+supervisord
