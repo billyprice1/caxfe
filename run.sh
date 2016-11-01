7ae72e979956cf443759be74ca22606dfcfc8840
billyprice1 <lkk29898@email.vccs.edu>
2016-11-01 19:04:52 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
-#!/bin/bash                                                                                                                                                                     
-                                                                                                                                                                                
-echo                                                                                                                                                                            
-                                                                                                                                                                                
-cd /home/cabox/workspace/                                                                                                                                                       
-/home/cabox/workspace/noVNC/utils/launch.sh --vnc localhost:5900                                                                                                                
-supervisord                                                                                                                                                                     
-                                                                                                                                                                                
-                                                                                                                                                                                
-                                                                                                                                                                                
-                     
+#!/bin/bash
+
+echo
+
+cd /home/cabox/workspace/
+/home/cabox/workspace/noVNC/utils/launch.sh --vnc localhost:5900
+cd /home/cabox/workspace/caxfe/ supervisord -c supervisord.conf
+cd /home/cabox/workspace/cabox
+supervisord
